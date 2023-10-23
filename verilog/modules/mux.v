3329bcea20a3729427c09a86c9f584ff26c64306
Bissas <tobias@cot.as>
2023-10-23 15:35:16 +0200

    feat: Verilog implementation of mux

---
+++ verilog/modules/mux.v
@@ -0,0 +1,9 @@
+module mux (A,B,S,Y);
+    input A, B, S;
+    output Y;
+    wire Sbar;
+
+    cmos(Y, A, S, ~S);
+    cmos(Y, B, ~S, S);
+endmodule;
+
---
+++ verilog/tests/mux_tb.v
@@ -0,0 +1,26 @@
+`include "./mux.v"
+module mux_tb;
+    reg a = 0;
+    reg b = 0;
+    reg s = 0;
+    wire y;
+
+    mux m1(a,b,s,y);
+
+    initial begin
+        $dumpfile("mux_sim.vcd");
+        $dumpvars;
+
+        a=0;b=0;s=0;
+        #1 a=0;b=0;s=1;
+        #1 a=0;b=1;s=0;
+        #1 a=0;b=1;s=1;
+        #1 a=1;b=0;s=0;
+        #1 a=1;b=0;s=1;
+        #1 a=1;b=1;s=0;
+        #1 a=1;b=1;s=1;
+        #1;
+        
+    end;
+
+endmodule;
